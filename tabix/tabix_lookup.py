8ceef93386dad5ba1e8a13705e86429c83fc5f4f
kleinone <kleinone@systemsbiology.org>
2014-03-12 16:47:52 -0700

    Added default handler for exceptions. Send response code depending on caught exception.

--- tabix/tabix_lookup.py
+++ tabix/tabix_lookup.py
@@ -5,18 +5,44 @@ import json
 
 from tabix_utils import tsv_region_lookup, vcf_singleline_lookup, triotype_singleline_lookup
 
+from tabix_utils import CoordinateRangeEmptyError, WrongLineFoundError, UnexpectedTabixOutputError
+
 class TabixLookupHandler(tornado.web.RequestHandler):
     def initialize(self):
         self._config_map = self.tabix_file_map
-    
+
+    def _handle_request_exception(self, error):
+        logging.error("Uncaught exception: " + str(error))
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 400:
+            self.write("Bad request")
+        else:
+            self.write("Server error occurred")
+
+    def build_response_object(self, chromosome, start, end, values=None, snpid="", ref="", alt="", info=""):
+        if values == None:
+            values = []
+
+        return {
+            "chr": chromosome,
+            "start": start,
+            "end": end,
+            "values": values,
+            "snpid": snpid,
+            "ref": ref,
+            "alt": alt,
+            "info": info
+        }
+
     def get(self, tabix_id, chromosome, start_coordinate, end_coordinate=None):
         if end_coordinate is None:
             end_coordinate = start_coordinate
         
         if tabix_id not in self._config_map.keys():
-            if options.verbose:
-                logging.info("Unknown tabix loookup ID [%s]" % tabix_id)
-            raise tornado.web.HTTPError(404)
+            logging.error("Unknown tabix lookup ID [%s]" % tabix_id)
+            self.send_error(400)
+            return
         
         file_info = self._config_map[tabix_id]
         file_path = file_info['path']
@@ -35,31 +61,38 @@ class TabixLookupHandler(tornado.web.RequestHandler):
             # flag has to be included in the command line.
             tabix_exe = options.tabix_executable + " -h"
         else:
-            logging.error("Unknown type for file " + file_path)
-            raise tornado.web.HTTPError(404)
+            logging.error("Unknown type for file in configuration: " + file_path)
+            self.send_error(500)
+            return
         
         try:
             result = lookup_fn(tabix_exe, file_path, chromosome, start_coordinate, end_coordinate)
-            response = {
-                "chr": result.chromosome,
-                "start": result.start,
-                "end": result.end,
-                "values": result.values,
-                "snpid": result.snpid,
-                "ref": result.ref,
-                "alt": result.alt,
-                "info": result.info
-            }
+            response = self.build_response_object(result.chromosome,
+                                                  result.start,
+                                                  result.end,
+                                                  values=result.values,
+                                                  snpid=result.snpid,
+                                                  ref=result.ref,
+                                                  alt=result.alt,
+                                                  info=result.info)
             
             self.write(json.dumps(response, sort_keys=True))
             self.set_status(200)
+
+        except CoordinateRangeEmptyError as cnf:
+            logging.info(cnf)
+            response = self.build_response_object(chromosome, start_coordinate, end_coordinate)
+            self.write(json.dumps(response, sort_keys=True))
+            self.set_status(200)
+
+        except WrongLineFoundError as wlf:
+            logging.error(wlf)
+            self.send_error(500)
+
+        except UnexpectedTabixOutputError as eto:
+            logging.error(eto)
+            self.send_error(500)
+
         except Exception as e:
-            logging.error("Running tabix failed:")
-            logging.error(e)
-            raise tornado.web.HTTPError(404)
-
-
-
-
-
-
+            logging.error("Running tabix failed: " + str(e))
+            self.send_error(500)
